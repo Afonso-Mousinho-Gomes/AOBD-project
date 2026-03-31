ef1382792878e4b8cd3710b8f946f61109f47045
Afonso Gomes <afonso.a.m.gomes@tecnico.ulisboa.pt>
2026-03-31 14:02:49 +0100

    UPDATE task3.sql for more credible scenarios

--- task3.sql
+++ task3.sql
@@ -1,3 +1,4 @@
+-- no index 11.515
 SELECT locality, AVG(price) AS average_new_property_price
 FROM ShortPricePaidData2025
 WHERE town = 'London'
@@ -6,25 +7,27 @@ GROUP BY locality
 ORDER BY average_new_property_price DESC;
 
 SELECT * FROM ShortPricePaidData2024
-
+--0.0352695
 CREATE INDEX idx_town_oldnew_locality
 ON ShortPricePaidData2025(town, old_new, locality)
 INCLUDE(price);
 
+--0.0805968
 CREATE INDEX idx_town_oldnew
 ON ShortPricePaidData2025(town, old_new)
 INCLUDE(price, locality);
 
-CREATE INDEX idx_town_inc_3
+--0.464414
+CREATE INDEX idx_town_inc
 ON ShortPricePaidData2025(town)
 INCLUDE(price, locality, old_new);
 
-CREATE INDEX idx_town
-ON ShortPricePaidData2025(town);
-
-CREATE INDEX idx_locality
-ON ShortPricePaidData2025(locality);
+--0.464414
+CREATE INDEX idx_oldnew
+ON ShortPricePaidData2025(town)
+INCLUDE(price, locality, old_new);
 
 DROP INDEX idx_town_oldnew_locality ON ShortPricePaidData2025;
 DROP INDEX idx_town_oldnew ON ShortPricePaidData2025;
-DROP INDEX idx_town_inc_3 ON ShortPricePaidData2025;
+DROP INDEX idx_town_inc ON ShortPricePaidData2025;
+DROP INDEX idx_oldnew ON ShortPricePaidData2025;
