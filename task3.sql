f9f1b22a99078d98ac23f74abed7c76ff4c01ba3
Afonso Gomes <afonso.a.m.gomes@tecnico.ulisboa.pt>
2026-04-02 03:24:38 +0100

    minor changes, task 3 now has relevant indexes

--- task3.sql
+++ task3.sql
@@ -6,28 +6,20 @@ AND old_new = 'Y'
 GROUP BY locality
 ORDER BY average_new_property_price DESC;
 
-SELECT * FROM ShortPricePaidData2024
---0.0352695
-CREATE INDEX idx_town_oldnew_locality
-ON ShortPricePaidData2025(town, old_new, locality)
-INCLUDE(price);
+--first index 5.72706
+CREATE INDEX first_idx
+ON ShortPricePaidData2025(town, old_new);
 
---0.0805968
-CREATE INDEX idx_town_oldnew
+--second index 0.0759047
+CREATE INDEX second_idx
 ON ShortPricePaidData2025(town, old_new)
 INCLUDE(price, locality);
 
---0.464414
-CREATE INDEX idx_town_inc
-ON ShortPricePaidData2025(town)
-INCLUDE(price, locality, old_new);
+--third index 0.0325524
+CREATE INDEX best_idx
+ON ShortPricePaidData2025(old_new, town, locality)
+INCLUDE(price);
 
---0.464414
-CREATE INDEX idx_oldnew
-ON ShortPricePaidData2025(old_new)
-INCLUDE(price, locality, town);
-
-DROP INDEX idx_town_oldnew_locality ON ShortPricePaidData2025;
-DROP INDEX idx_town_oldnew ON ShortPricePaidData2025;
-DROP INDEX idx_town_inc ON ShortPricePaidData2025;
-DROP INDEX idx_oldnew ON ShortPricePaidData2025;
+DROP INDEX first_idx ON ShortPricePaidData2025;
+DROP INDEX second_idx ON ShortPricePaidData2025;
+DROP INDEX best_idx ON ShortPricePaidData2025;
